b8b2d2edf97beedbee1db2727b4a3625417b5783
cristina guirado boluda <cguirado@zeta40.aulas.gsyc.es>
2015-09-21 10:57:34 +0200

    Añadido - * sin probar

--- calc.py
+++ calc.py
@@ -2,9 +2,20 @@
 # -*- coding: utf-8 -*-
 
 # Vamos a programar una calculadora
-
+import sys 
 def main():
     pass
+def suma(sumando1,sumando2):
+    return sumando1 + sumando2
+def resta(resta1,resta2):
+    return restador1 - restador2
+def multiplicacion (multiplicador1,multiplicador2):
+    return multiplicador1 * multiplicador2
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        operando1=int(sys.argv[2])
+        operando2=int(sys.argv[3])
+    except ValueError:
+        print("Tiene que ser enteros!")

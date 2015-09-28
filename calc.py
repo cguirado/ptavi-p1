5d7f8e362041e7d582b28c7665146a8709f1cc76
cristina guirado boluda <cguirado@zeta40.aulas.gsyc.es>
2015-09-28 11:20:46 +0200

    Añadido - * Hecha no funciona

--- calc.py
+++ calc.py
@@ -11,11 +11,22 @@ def resta(resta1,resta2):
     return restador1 - restador2
 def multiplicacion (multiplicador1,multiplicador2):
     return multiplicador1 * multiplicador2
-
+def division (dividiendo,divisor):
+    return dividendo / divisor
 
 if __name__ == "__main__":
     try:
         operando1=int(sys.argv[2])
-        operando2=int(sys.argv[3])
+        operando2=int(sys.argv[4])
+        operacion=sys.argv[3]
     except ValueError:
-        print("Tiene que ser enteros!")
+        sys.exit("Tiene que ser enteros!")
+    if operacion == 'suma'
+        print(suma(operacion1,operacion2))
+    elif operacion == 'resta'
+        print(resta(operacion1,opreacion2)) 
+    elif operacion == 'multiplicacion'
+        print(multiplicacion(operacion1,opreacion2))
+    elif operacion == 'division'
+        print(division(operacion1,opreacion2))
+
